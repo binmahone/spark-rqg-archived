8c7703937ba1c8ac3512007b46ae15a52217849f
Liu,Linhong <liulinhong@baidu.com>
2020-02-23 21:23:45 +0800

    issue #46: WhereClause should not generate aggreate expression
    Closes #46

--- src/main/scala/org/apache/spark/rqg/ast/clauses/WhereClause.scala
+++ src/main/scala/org/apache/spark/rqg/ast/clauses/WhereClause.scala
@@ -2,7 +2,7 @@ package org.apache.spark.rqg.ast.clauses
 
 import org.apache.spark.rqg.{BooleanType, RQGConfig, RandomUtils}
 import org.apache.spark.rqg.ast.expressions.BooleanExpression
-import org.apache.spark.rqg.ast.{QuerySession, TreeNode, TreeNodeGenerator}
+import org.apache.spark.rqg.ast.{AggPreference, QuerySession, TreeNode, TreeNodeGenerator}
 
 /**
  * whereClause
@@ -18,6 +18,7 @@ class WhereClause(
   private def generateBooleanExpression = {
     val (min, max) = querySession.rqgConfig.getBound(RQGConfig.MAX_NESTED_EXPR_COUNT)
     querySession.allowedNestedExpressionCount = RandomUtils.choice(min, max)
+    querySession.aggPreference = AggPreference.FORBID
     BooleanExpression(querySession, Some(this), BooleanType, isLast = true)
   }
   override def sql: String = s"WHERE ${booleanExpression.sql}"
